b3ca714050876ab013ef7c163bdcf8f388023e2a
Matt Bedell <matt@bedell.co>
2018-10-21 16:44:04 -0400

    basic paragraph and sentinel components

---
+++ src/components/Paragraph.vue
@@ -0,0 +1,19 @@
+<template>
+  <span>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ut, debitis, minima repudiandae vitae impedit beatae repellendus, velit voluptatem ex id veniam dolorum vel! Explicabo dolorem vero tenetur quidem voluptatem?
+    </p>
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    paragraphIndex: {
+      type: Number,
+      required: true,
+    }
+  },
+}
+</script>
---
+++ src/components/Sentinel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :id="sId" :class="{'vis-hide': !visible}"></div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      sentId: this.sId,
+    }
+  },
+  props: {
+    // id must be provided
+    sId: {
+      type: String,
+      required: true,
+    },
+    visible: {
+      type: Boolean,
+    }
+  },
+}
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>

4595eac4f1f326ad33c7bee73cdc3f88f844151f
Matt Bedell <matt@bedell.co>
2018-10-21 16:42:52 -0400

    add placeholder adunits

---
+++ src/config/adUnits.js
@@ -0,0 +1,104 @@
+export default [
+  {
+    code: 'header-bid-tag-0',
+    adPath: '/19968336/header-bid-tag-0',
+    mediaTypes: {
+      banner: {
+        sizes: [
+          [300, 250],
+        ],
+      },
+    },
+    bids: [{
+      bidder: 'appnexus',
+      params: {
+        placementId: 13144370,
+      },
+    }],
+  },
+  {
+    code: 'header-bid-tag-1',
+    adPath: '/19968336/header-bid-tag-1',
+    mediaTypes: {
+      banner: {
+        sizes: [
+          [300, 250],
+        ],
+      },
+    },
+    bids: [{
+      bidder: 'appnexus',
+      params: {
+        placementId: 13144370,
+      },
+    }],
+  },
+  {
+    code: 'header-bid-tag-2',
+    adPath: '/19968336/header-bid-tag-2',
+    mediaTypes: {
+      banner: {
+        sizes: [
+          [300, 250],
+        ],
+      },
+    },
+    bids: [{
+      bidder: 'appnexus',
+      params: {
+        placementId: 13144370,
+      },
+    }],
+  },
+  {
+    code: 'header-bid-tag-3',
+    adPath: '/19968336/header-bid-tag-3',
+    mediaTypes: {
+      banner: {
+        sizes: [
+          [300, 250],
+        ],
+      },
+    },
+    bids: [{
+      bidder: 'appnexus',
+      params: {
+        placementId: 13144370,
+      },
+    }],
+  },
+  {
+    code: 'header-bid-tag-4',
+    adPath: '/19968336/header-bid-tag-4',
+    mediaTypes: {
+      banner: {
+        sizes: [
+          [300, 250],
+        ],
+      },
+    },
+    bids: [{
+      bidder: 'appnexus',
+      params: {
+        placementId: 13144370,
+      },
+    }],
+  },
+  {
+    code: 'header-bid-tag-5',
+    adPath: '/19968336/header-bid-tag-5',
+    mediaTypes: {
+      banner: {
+        sizes: [
+          [300, 250],
+        ],
+      },
+    },
+    bids: [{
+      bidder: 'appnexus',
+      params: {
+        placementId: 13144370,
+      },
+    }],
+  },
+];
